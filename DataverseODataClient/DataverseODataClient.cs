ac064176d2ea00c06b2bc553c01ffdc4efee3597
Lars Bauer <LA_BA@ec4u.de>
2021-02-05 16:56:14 +0100

    fix dataverse client visibility

--- DataverseODataClient/DataverseODataClient.cs
+++ DataverseODataClient/DataverseODataClient.cs
@@ -3,7 +3,7 @@
 namespace DataverseODataClient
 {
     // this class is needed to enable dependency injection of ODataClientSettings
-    internal class DataverseODataClient : ODataClient
+    public class DataverseODataClient : ODataClient
     {
         public DataverseODataClient(ODataClientSettings clientSettings)
             : base(clientSettings)
