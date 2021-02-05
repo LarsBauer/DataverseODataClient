7571caaff5e0d2a6c524d9fd6afade1a7ab14012
Lars Bauer <LA_BA@ec4u.de>
2021-02-05 11:55:22 +0100

    xmldoc for options

--- DataverseODataClient/DataverseODataClientOptions.cs
+++ DataverseODataClient/DataverseODataClientOptions.cs
@@ -2,9 +2,20 @@
 
 namespace DataverseODataClient
 {
+    /// <summary>
+    /// Dataverse OData client configuration options
+    /// </summary>
     public class DataverseODataClientOptions
     {
+        /// <summary>
+        /// The base url of your Dataverse environment in the following format:
+        /// https://{{organizationName}}.crm4.dynamics.com/api/data/v9.1/
+        /// </summary>
         public Uri OrganizationUrl { get; set; }
+
+        /// <summary>
+        /// When using a user-assigned managed identity in Azure you have to specifiy the client id
+        /// </summary>
         public string ManagedIdentityClientId { get; set; }
     }
 }
